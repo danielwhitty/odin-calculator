2d781dccc8d0e2dc58d43e302595a12bcab71f98
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-28 22:41:37 +1000

    Add logic to change calculator display

--- scripts.js
+++ scripts.js
@@ -17,3 +17,19 @@ function divide(a, b) {
 function operate(func, a, b) {
     return func(a, b);
 }
+
+// Add elements and function to clear display
+const display = document.getElementById('display');
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', function() {
+    display.textContent = '';
+});
+
+// Add to display
+const numbers = document.querySelectorAll('.numbers button');
+for (let number of numbers) {
+    number.addEventListener('click', function() {
+        display.textContent += number.textContent;
+    });
+}
